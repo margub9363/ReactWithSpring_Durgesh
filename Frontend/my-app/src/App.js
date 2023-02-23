f649cac2d2b01e44ffc30344c56e860631660e49
rahman <margub.9363@gmail.com>
2023-02-23 10:12:07 +0530

    47.00

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <h1>This is my react app</h1>
+      <p>This is my program. I am currently learning react js</p>
+      <p>Beacuse I want to develop application that calls my REST api.</p>
     </div>
   );
 }
---
+++ Frontend/my-app/src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <p>This is header for learning purpose</p>
+    </div>
+  );
+}
+
+export default Header;
