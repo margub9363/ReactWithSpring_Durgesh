925f817b5b261d9aee8fd8104f303c12a889c534
rahman <margub.9363@gmail.com>
2023-02-23 22:17:05 +0530

    Project Ready

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,16 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <Header name="Tannu" />
-      <Header name="Margub" />
-      <Header name="Rahman" />
-      <h1>This is my react app</h1>
-      <p>This is my program. I am currently learning react js</p>
-      <p>Beacuse I want to develop application that calls my REST api.</p>
+      <h1>Simple application</h1>
     </div>
   );
 }
