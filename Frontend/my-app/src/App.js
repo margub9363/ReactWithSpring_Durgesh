15899c996d1328ea4552d5b85cba55404b263430
rahman <margub.9363@gmail.com>
2023-02-24 22:49:22 +0530

    1:47

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -5,17 +5,23 @@ import { ToastContainer, toast } from "react-toastify";
 import Home from "./components/Home";
 import Course from "./components/Course";
 import AllCourse from "./components/Allcourses";
+import AddCourse from "./components/AddCourse";
 
 function App() {
   const btnHandle = () => {
-    toast.success("This is my first message");
+    toast.error("done", {
+      position: "top-center",
+    });
   };
+
   return (
     <div>
       <ToastContainer />
       <h1>This is bootstrap Component</h1>
       <Home />
       <AllCourse />
+
+      <AddCourse />
     </div>
   );
 }
