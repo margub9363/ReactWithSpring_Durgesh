c21d0956d806dcee4b2695cfeedda6350334a839
rahman <margub.9363@gmail.com>
2023-02-26 00:20:59 +0530

    2:41 Api call

--- Frontend/my-app/src/components/Allcourses.js
+++ Frontend/my-app/src/components/Allcourses.js
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Course from "./Course";
+import base_url from "../api/bookApi";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const AllCourse = (course) => {
+  // calling loading course  function
+  useEffect(() => {
+    getAllCourseFromServer();
+  });
+
   const [courses, setCourses] = useState([
     { title: "Java", description: "Basic Java Course" },
     { title: "Python", description: "Basic Python Course" },
@@ -11,6 +19,26 @@ const AllCourse = (course) => {
   useEffect(() => {
     document.title = "All Course";
   }, []);
+
+  // function to call server
+  const getAllCourseFromServer = () => {
+    console.log(base_url);
+    axios.get(`${base_url}/courses`).then(
+      (success) => {
+        // success
+        console.log(success.data);
+        toast.success("Success", {
+          position: "bottom-center",
+        });
+      },
+      (error) => {
+        console.log(error);
+        toast.error("Something went wrong", {
+          position: "bottom-center",
+        });
+      }
+    );
+  };
 
   return (
     <div>
